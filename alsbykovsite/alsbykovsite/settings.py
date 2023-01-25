91937d1999b3d1fdb8f79fbc51556616737b1a0c
Alex Bykov <bikov23alex@gmail.com>
2023-01-25 18:23:37 +0500

    fix media path

--- alsbykovsite/alsbykovsite/settings.py
+++ alsbykovsite/alsbykovsite/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = '/home/alsbykov/urfu-django/static'
+STATICFILES_DIRS = '/home/alsbykov/urfu-django/albykovsite/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/alsbykov/urfu-django/media'
+MEDIA_ROOT = '/home/alsbykov/urfu-django/albykovsite/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
