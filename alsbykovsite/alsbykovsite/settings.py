2f635637ebfbf874b29669163f255f31c8c73b80
Alex Bykov <bikov23alex@gmail.com>
2023-01-25 18:30:31 +0500

    fix media path

--- alsbykovsite/alsbykovsite/settings.py
+++ alsbykovsite/alsbykovsite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = str(BASE_DIR / "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR / "media")
